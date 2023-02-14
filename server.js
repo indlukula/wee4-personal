45e76f002b3bba0fd72bb6f5ebcdf32eb076c550
indlukula <84289216+indlukula@users.noreply.github.com>
2023-02-15 01:30:02 +0200

    Installed cors

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Conten-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
   })
